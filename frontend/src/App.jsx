4bb996408a6d70dc7bc4dc8a671876eacddfc03c
md-sohail <md.sohail@hpe.com>
2023-08-20 04:01:17 +0530

    Delete frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,8 @@
 import React, { useEffect, useState } from "react";
-import TopBar from "./component/TopBar.jsx";
-import SideBar from "./component/SideBar.jsx";
-import Table from "./component/Table.jsx";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
 
 function App() {
-	const [theme, colorMode] = useMode();
-	const [isSidebar, setIsSidebar] = useState(true);
 	return (
-		<ColorModeContext.Provider value={colorMode}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<div className="app">
-					<SideBar isSidebar={isSidebar} />
-					<main className="content">
-						<TopBar setIsSidebar={setIsSidebar} />
-						<Table />
-					</main>
-				</div>
-			</ThemeProvider>
-		</ColorModeContext.Provider>
+		<h1>Hello world</h1>
 	);
 }
 
